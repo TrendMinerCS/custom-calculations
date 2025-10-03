f2c1b3668fce429eeeb0ed2ce0380739b9c06ae5
Wouter Daniels <wouter.daniels@trendminer.com>
2025-10-03 09:39:30 +0200

    Avoid errors from concatenating empty lists

--- custom_calculations_scripts/search_results_examples/incrementing_value_totalizer_search_results.py
+++ custom_calculations_scripts/search_results_examples/incrementing_value_totalizer_search_results.py
@@ -81,19 +81,21 @@ for interval in intervals:
 
     ser_list.append(totals)
 
-# Concatenate the series
-ser = pd.concat(ser_list)
-ser.name = "value"
+# only proceed if the list is not empty
+if ser_list:
+    # Concatenate the series
+    ser = pd.concat(ser_list)
+    ser.name = "value"
 
-# Filter for timestamps and NaN values
-ser = (
-    ser
-    .loc[lambda x: x.index >= index_interval.start]
-    .loc[lambda x: x.index < index_interval.end]
-    .dropna()
-)
+    # Filter for timestamps and NaN values
+    ser = (
+        ser
+        .loc[lambda x: x.index >= index_interval.start]
+        .loc[lambda x: x.index < index_interval.end]
+        .dropna()
+    )
 
-# To file
-ser.to_csv(
-    os.environ["OUTPUT_FILE"]
-)
+    # To file
+    ser.to_csv(
+        os.environ["OUTPUT_FILE"]
+    )
